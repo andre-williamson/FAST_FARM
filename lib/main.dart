3e2527723d67df8a87fed2d2d97a5c0a9b28f20e
FlutLab.io <>
2022-02-28 00:01:05 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.purple,
       ),
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'FASTFARM'),
     );
   }
 }
